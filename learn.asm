6e26fc4458303527dd185b07577e968321733bc7
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2024-10-22 13:02:41 +0300

    Create learn.asm

---
+++ learn.asm
@@ -0,0 +1,2 @@
+;Before starting  - define instruction set for your CPU by directives (THUMB or ARM)
+;there can be THUMB or ARM instruction set. When core runs with THUMB , it can`t runs any ARM instruction

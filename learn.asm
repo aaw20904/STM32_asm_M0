c54b8c439c8baf967e8985cfdab7c56bef257665
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2024-10-22 13:03:39 +0300

    Update learn.asm

--- learn.asm
+++ learn.asm
@@ -1,2 +1,2 @@
-;Before starting  - define instruction set for your CPU by directives (THUMB or ARM)
+;Before starting  - define instruction set for your CPU by directives (THUMB or ARM or  ThumbEE)
 ;there can be THUMB or ARM instruction set. When core runs with THUMB , it can`t runs any ARM instruction

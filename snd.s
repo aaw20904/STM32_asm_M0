8c117d0ee5080f46f35a363818fe2496128208bc
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-01-01 23:08:59 +0300

    Add files via upload

--- snd.s
+++ snd.s
@@ -43,7 +43,7 @@ __Vectors       DCD     top_of_stack  ; Top of Stack $00000000
                 DCD     Def_Vec   ; DMA1 Channel 3  $74
                 DCD     Def_Vec   ; DMA1 Channel 4  $78
                 DCD     Def_Vec   ; DMA1 Channel 5  $7C
-                DCD     Def_Vec   ; DMA1 Channel 6   $80
+                DCD     DMA1_CH6_ISR   ; DMA1 Channel 6   $80
                 DCD     DMA1_CH7_ISR   ; DMA1 Channel 7   $84
                 DCD     Def_Vec   ; ADC1_2         $88   
                 DCD     Def_Vec    ; USB High Priority or CAN1 TX  $8C
@@ -55,8 +55,8 @@ __Vectors       DCD     top_of_stack  ; Top of Stack $00000000
                 DCD     Def_Vec   ; TIM1 Update                  $A4
                 DCD     Def_Vec   ; TIM1 Trigger and Commutation      $A8
                 DCD     Def_Vec    ; TIM1 Capture Compare         $AC
-                DCD     tim2UpdateISR   ; TIM2                      $B0
-                DCD     Def_Vec    ; TIM3                      $B4
+                DCD     Def_Vec   ; TIM2                      $B0
+                DCD     tim3UpdateISR    ; TIM3                      $B4
                 DCD     Def_Vec    ; TIM4                       $B8
                 DCD     Def_Vec    ; I2C1 Event                   $BC
                 DCD     Def_Vec    ; I2C1 Error                   $C0
@@ -67,29 +67,49 @@ __Vectors       DCD     top_of_stack  ; Top of Stack $00000000
                 DCD     Def_Vec    ; USART1                      $D4
                 DCD     usart2ISR    ; USART2                      $D8
                 DCD     Def_Vec    ; USART3                      $DC
-                DCD     Def_Vec   ; EXTI Line 15..10               $E0
+                DCD     gpioInterruptISR  ; EXTI Line 15..10               $E0
                 DCD     Def_Vec   ; RTC Alarm through EXTI Line   $E4
                 DCD     Def_Vec    ; USB Wakeup from suspend        $E8
 					
    AREA myConst , DATA, READONLY 
 prompt   DCB "Donald Duck is our favorite candidate in President election!!!!!!!!!!!"	
+   
+   AREA glVariables, DATA, READWRITE
+uartRxBuffer      SPACE 32	
+uartTxBuffer      SPACE 32	
+dma1_7_disable_ch SPACE 4
+dma1_7_data_to_transmit SPACE 4
+semaphore         SPACE 4
+byteCounter       SPACE 4
+bytePointer       SPACE 4
+
 
    AREA MainCode, CODE, READONLY
 	  ; |.text|
 	  INCLUDE library.s  
-  
    ENTRY
    
 Start         PROC
-     LDR r0, =RCC_APB2ENR
+
+	  LDR R1, =0x00000000
+	  LDR R0, =semaphore
+	  STR R1, [R0]
+	  LDR R0, =byteCounter
+	  LDR R1, =0x00000008
+	  STR R1, [R0] ;init byte counter
+	  LDR R1, =0x00000000
+      LDR r0, =RCC_APB2ENR
+	  STR R1, [R0]  ;clear APB2ENR
+	  
+	  LDR R1, =uartTxBuffer
+      LDR R0, =bytePointer
+      STR R1, [R0]	;initialize pointer  
 	 
 	  BL gpio_init     ; Call the gpio_init procedure from the other file
-	  LDR R0, =0x00300000  ;MODE C13=11 (bits20,21),   
-	  LDR R1, =GPIOC_CRH 
-	  STR R0, [R1]
+	 
 	  ;--
 	  LDR R0, =GPIOA_CRH
-	  LDR R1,=0x0000000B
+	  LDR R1, =0x0000000B
 	  STR R1,[R0]
 	  ;PA1 - alternative function TIM2_CH2
 	  LDR R0, =GPIOA_CRL
@@ -97,12 +117,34 @@ Start         PROC
 	  STR R1, [R0]
 	  ;PORT B h
 	  LDR R0, =GPIOB_CRH
-	  LDR R1, =0x00030000
+	  LDR R1, =0x44444444
 	  STR R1,[R0]
 	  ;PORTB low
 	  LDR R0, =GPIOB_CRL
-	  LDR R1,=0x00003000
+	  LDR R1,=0x44444444
 	  STR R1, [R0]
+	  ;---
+	  LDR R1, =0x04300000  ;MODE C13=11 (bits20,21),   
+	  LDR R0, =GPIOC_CRH 
+	  STR R1, [R0]
+	  ;Map GPIOC14 to EXTI Line 14
+	  LDR R0, =AFIO_EXTICR4  
+	  LDR R1, =0x00000200 ;mapping GPIOC14 to EXTI14  
+	  STR R1, [R0]
+	  ;Configure EXTI for Line 14
+	  LDR R0, =EXTI_IMR       ; EXTI base address
+      LDR R1, =(1 << 14) ;enable bit 14
+	  STR R1, [R0];
+	  
+	  LDR R0, =EXTI_RTSR
+	  LDR R1, =(1 << 14)  ; bit 14 on rising edge
+	  STR R1, [R0]
+	  ;Enable EXTI14 Interrupt in NVIC
+	   LDR R0, =NVIC_ISER1        ; NVIC_ISER1 address (for IRQ numbers 32-63)
+       LDR R1, =(1 << (40 - 32)) ; Enable IRQ40 (EXTI14 is IRQ40)
+       STR R1, [R0]              ; Write to NVIC_ISER1
+	  
+	  
 	  ;----------------------C L O C K --------------------
 	  ;---------sys_clock = (12 288 000Hz / 2) * 5 = 30 720 000 
 	  LDR R5, =0x03010001
@@ -138,19 +180,27 @@ Start         PROC
 	    ;PUSH {R0,R1}
 	    ;BL uart_init_tx
 	   
-;=======FUNCTION uart_init_rx with Interrupt
+;=======FUNCTION uart_init_rx with DMA
 ;--par1@[WordLength(8)|stopBits(8)|bauds_divider(16)]  
-;par2@[interrupt_tx_enable(8)|interrupt_rx_enable(8)|parity_type(8)|prity_en(8)]
+;--par2@[-|interrupt_RX_enable(8)|parity_type(8)|prity_en(8)]
+;--par3@[pointerToDmaBuffer]   
+;--par4@[null(4)|DMA_TC_interrupt(4)|priority_channel(8)|buffer_size(16)]
 ;WordLength: WHEN 1 -> 1 Start bit, 9 Data bits, n Stop bit
 ;            WHEN 0-> 0: 1 Start bit, 8 Data bits, n Stop bit
 ;StopBits: 0b00-> 1 bit, 0b01-> 0.5 Stop bit, b10-> 2 Stop bits, b11-> 1.5 Stop bit,
-;divider = [whole_part_divider | b3 remainder b0]
 ;parity_en:   0-> disable parity, 1-> enable parity
 ;parity_type: 1->odd, 0->even
+;interrupt_enable: FF->enable
        ;LDR R0, =0x00000C80	   
-	   ;LDR R1, =0x00FF0000
-	   ;PUSH {R0,R1}
-	   ;BL uart_init_duplex
+	   ;LDR R1, =0x00000000
+	   ;LDR R2, =uartRxBuffer
+	   ;LDR R3, =0x0F010020
+	   ;PUSH {R0,R1,R2,R3}
+	   ;BL uart_init_rx_dma
+	   ;LDR R0,=DMA1_CCR6
+       ;LDR R2, =0x1
+       ;LDR R1,[R0]
+       ;ORR R1,R2
 	   
 ;=======USART2_TRANSMITTER_DRIVEN_DMA==============
 ;--par1@[WordLength(8)|stopBits(8)|bauds_divider(16)]  
@@ -165,78 +215,192 @@ Start         PROC
 ;NOTES: 1) After a full DMA transaction content of the DMA1_CNDTR7 reister reaches zero
 ;It MUST be restored before start the next DMA transaction
 ;     2)To start DMA TX transaction - set bit 0 in DAM_CCR7 channel    
-  LDR R0, =0x00000C80
-  LDR R1, =0x000000ff;
-  LDR R2, =prompt
-  LDR R3, =0x01010040 ;64bytes TX ROM buffer
+  LDR R0, =0x00000035 ;576 KBaud
+  LDR R1, =0x00000000;
+   LDR R2, =uartTxBuffer ;buffer address
+   LDR R3, =0x00000008 ;8bytes TX ROM buffer
   PUSH {R0,R1,R2,R3}
   BL uart_init_tx_dma
-  LDR R0,=DMA1_CCR7
-  LDR R2, =0x1
-  LDR R1,[R0]
-  ORR R1,R2
-  STR R1,[R0]
-	    ;LDR R2, =GPIOC_BSRR
-	   ; LDR R1, =0x007A1200;
-	    ;PUSH {R1,R2}
-	    ;BL tim2SimpleModeInitInt
+  ;LDR R0,=DMA1_CCR7
+  ;LDR R2, =0x1
+  ;LDR R1,[R0]
+  ;ORR R1,R2
+  ;STR R1,[R0]
+  
+	;====FUNCTION initialize TIM2 CH2 Output Compare
+	;    A@[b31 presc| period b0],
+	;Width@[b31 null |  width b0]
+	;example bassing params PUSH {A,Width}
+		LDR R1, =0x00000027 ;384kHz
+		LDR R2, =0x18; //align edges
+		PUSH {R1,R2}
+		BL tim2OcCh2Setup
+		
+	 ;FUNCTION initialization counter in simple mode with interrupt	
+	;parameters:  @32bit - [  prescaler(16)| counter(16) b0]  ,@32bit-null	
+		LDR R2, =0x00000000
+		LDR R1, =0x0000004f;0x4F - 384kHz
+		PUSH {R1,R2}
+		BL tim3SimpleModeInitInt
+		;clear registers
+		MOV R1, #0x0
+		MOV R4,R1 ;low 64 bit-frame (bitstream buffer)
+		MOV R5,R1 ;high 64 bit-frame (bitstream buffer)
+		MOV R1, #0x41 ;count of bits(64)
+		MOV R6,R1  ;bit counter
 mylabel
-    ;WFI
+     ;WFI
+	 LDR R0,=semaphore
+	 LDR R1,[R0]
+	 TST R1, R1
+	 BEQ mylabel
+	 LDR R1,=0x0
+	 STR R1,[R0] ;clear semaphore
+	;start DMA transmitting
+	 ;--start transaction
+	 
+	 LDR R0, =DMA1_CCR7
+	 LDR R1, [R0] ;load current 
+	 LDR R2, =0xFFFFFFFE 
+	 AND R1, R2 ;disable channel
+	 STR R1, [R0] ;store
+	 LDR R0, =DMA1_CNDTR7
+	 LDR R1, =0x8
+	 STR R1, [R0]
+	 LDR R0,=DMA1_CCR7
+	 LDR R1, =0x1
+	 LDR R2,[R0]
+	 ORR R1,R2
+	 STR R1,[R0]
+	 
 	B mylabel
 	ENDP
 ;---data---area
 
 		
 		
- ;ISR handlers---------
+ ;----ISR-handlers---------
  ;============default ISR handler.Infinite loop
 Def_Vec PROC
     B   .  ; Infinite loop
     ENDP
-;====TIM2=====ISR========		
-tim2UpdateISR   PROC
-	;clear flag UIE
-	LDR R0, =TIM2_SR
-	LDR R1, [R0]  ;load current content
-	BIC R1, R1, #0x01    ; Clear UIF flag (bit 0)
-	STR R1, [R0] ; update the SR
-	;---LED BLINK--BEGIN
-	;is the led in high (PC13) ?
-	LDR R0, =GPIOC_IDR
+;====ext vector interrupt
+gpioInterruptISR
+    LDR R0, =EXTI_PR           ; Load EXTI Pending Register address
+    LDR R1, =(1 << 14)         ; Prepare mask for EXTI Line 14
+    STR R1, [R0]               ; Write to EXTI_PR to clear the pending flag
+	;---
+	;has a byte counter reached zero?
+	LDR R0, =byteCounter
 	LDR R1, [R0]
-	LDR R3, =(1 << 13)
-	ANDS R1, R1, R3
-	BEQ t001_cleared
-	;when bit set - clear it
-	  LDR R0, =GPIOC_BSRR
-	  LDR R1, =0x20000000 ; bit13  set b13 ($2000), reset b29 ($20000000)
-	  STR R1, [R0]
-	  B t001_end
-t001_cleared
-      ;when bit cleared -set in to 1
-      LDR R0, =GPIOC_BSRR
-	  LDR R1, =0x00002000 ; bit13  set b13 ($2000), reset b29 ($20000000)
-	  STR R1, [R0]
-	  B t001_end      	  
-t001_end
-     ;---LED-BLINK---END
-	  LDR R0, =USART2_DR
-	  LDR R1, =0x30
-	  STR R1, [R0]
+	TSTEQ R1, R1
+	BNE labAllBytesReady
+	;-not all bytes ahs been received in DMA buffer
+	
+	BX LR
+labAllBytesReady	
+	;--DMA buffer is full
+	
+		;---T E S T----LED BLINK--BEGIN
+	 LDR R0, =GPIOC_BSRR
+     LDR R1, =(1 << 29)
+	 STR R1,[R0]
+	 NOP
+	 NOP
+	 NOP
+	 NOP
+	 NOP
+	 LDR R1, =(1 << 13)
+	 STR R1,[R0]
+    ;-T E S T ---LED-BLINK---END
+	BX LR
+;====TIM2=====ISR========		
+tim3UpdateISR   PROC
+	;clear flag UIE
+	LDR R0, =TIM3_SR
+    MOV R1,#0
+	STR R1, [R0] ; update the SR
+
+	;--delta-sigma---------adc---
+	;---[R5,R4] -bit stream buffer
+	;-R6 -bit counter, when 64bit reached-
+	;R4,R5 copying into RAM,and start DMA transmitting
+    ;through uart2
+     SUBS R6,#0x1
+	 BNE ds_buffer_not_full
+	 ;test begin
+	  ;LDR R4, =0xf0f0f0f0
+	  ;LDR R5, =0xf0f0f0f0
+	 ;test end 
+	 ;store regs to RAM
+	 LDR R0, =uartTxBuffer
+	 STR R4, [R0] ;save LOW
+	 ADD R0, #0x4
+	 STR R5, [R0] ;save HIGH
+	 LDR R6, =0x41 ;restore counter
+	 LDR R0, =semaphore;
+	 MOV R1, #0x1
+	 STR R1,[R0]
+ds_buffer_not_full
+     LDR R0,=GPIOB_IDR ;read current bit
+	 LDR R1,[R0]
+	 LSL R1,#0x17
+	 MOV R0,#0x80000000
+	 ANDS R1,R0 ;input bit (GPIOB 8) in R1 now as bit31 
+	 ;i n f o  "AND(S)"-> ‘S’ is an optional suffix. If S is specified, the condition code flags are updated on the
+     ;  result of the operation (see Conditional execution on page 56).
+	 LSR R4,#0x1 ;shifting right LOW 32bits
+	 LSRS R5,#0x1 ;shifting  HIGH 32bits , and result carry flag
+	 BCC ds_no_bit_carry  ;has carry been happened?
+	  ORR R4, #0x80000000 ;set bit31 when carry
+ds_no_bit_carry
+     ORR R5, R1  ;apply input bit from the port 
+    
+
+     ;--start transaction
+	 
+	 ;LDR R0, =DMA1_CCR7
+	 ;LDR R1, [R0] ;load current 
+	 ;LDR R2, =0xFFFFFFFE 
+	 ;AND R1, R2 ;disable channel
+	 ;STR R1, [R0] ;store
+	 ;LDR R0, =DMA1_CNDTR7
+	 ;LDR R1, =0x40
+	 ;STR R1, [R0]
+	 ;LDR R0,=DMA1_CCR7
+	 ;LDR R1, =0x1
+	 ;LDR R2,[R0]
+	 ;ORR R1,R2
+	 ;STR R1,[R0]
+	 
 	BX LR ;reti
 	ENDP
 ;=====USART2==Interrupt Service Routine=
 usart2ISR  PROC
 	
-	LDR  R0,=USART2_DR
-	LDR R1,[R0]
-	STR R1,[R0]
+	;LDR  R0,=USART2_DR
+	;LDR R1,[R0]
+	;STR R1,[R0]
 	BX LR
 	  ENDP
 ;==========DMA1 CH7 (UART2 TX)
 DMA1_CH7_ISR PROC
+	;clear flag 
+	LDR R0,=DMA1_IFCR
+	LDR R1,=(1<<24)
+	STR R1,[R0]
+	
    BX LR
    ENDP
+;====DMA1 Ch6 (USART2 RX)
+DMA1_CH6_ISR PROC
+	;clear flag 
+  ;clear
+	LDR R0,=DMA1_IFCR
+	LDR R1,=(1<<20)
+	STR R1,[R0]
+    BX LR
+         ENDP
 ;======hard fault ISR
 TRASH_ISR PROC
 	B .
